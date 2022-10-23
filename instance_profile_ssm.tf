d606a53cb7e5adc4b5a01db76e4def34c7f01653
Samuel Loh Yin Kang <samuel.loh.yk@gmail.com>
2022-10-23 21:27:50 +0800

    Fixing Instance Profile names
    Signed-off-by: Samuel Loh Yin Kang <samuel.loh.yk@gmail.com>

--- instance_profile_ssm.tf
+++ instance_profile_ssm.tf
@@ -1,10 +1,10 @@
 resource "aws_iam_role" "instance_profile_iam_role" {
-  name               = "${var.name}-instance-profile"
+  name               = "${var.name}-iam-role"
   assume_role_policy = data.aws_iam_policy_document.ssm_instance_profile_iam_role_assume_role_policy.json
 }
 
 resource "aws_iam_instance_profile" "ssm_instance_profile" {
-  name = "ssm_instance_profile"
+  name = "${var.name}-instance-profile"
   role = aws_iam_role.instance_profile_iam_role.name
 }
 
@@ -27,8 +27,8 @@ resource "aws_iam_role_policy_attachment" "ssm_instance_profile_iam_role_ssm_ins
 // https://docs.aws.amazon.com/systems-manager/latest/userguide/getting-started-create-iam-instance-profile.html#create-iam-instance-profile-ssn-logging
 data "aws_iam_policy_document" "ssm_instance_profile_iam_policy" {
   statement {
-    sid    = "AllowDownloadFromSsmS3Buckets"
-    effect = "Allow"
+    sid     = "AllowDownloadFromSsmS3Buckets"
+    effect  = "Allow"
     actions = [
       "s3:GetObject"
     ]
@@ -46,8 +46,8 @@ data "aws_iam_policy_document" "ssm_instance_profile_iam_policy" {
   }
 
   statement {
-    sid    = "AllowLoggingToCloudWatchLogGroup"
-    effect = "Allow"
+    sid     = "AllowLoggingToCloudWatchLogGroup"
+    effect  = "Allow"
     actions = [
       "logs:CreateLogStream",
       "logs:PutLogEvents",
@@ -61,21 +61,22 @@ data "aws_iam_policy_document" "ssm_instance_profile_iam_policy" {
   }
 
   statement {
-    sid    = "AllowLoggingToS3Bucket"
-    effect = "Allow"
+    sid     = "AllowLoggingToS3Bucket"
+    effect  = "Allow"
     actions = [
       "s3:PutObject",
       "s3:GetEncryptionConfiguration"
     ]
     resources = [
       "arn:aws:s3:::${var.s3_bucket_ssm_logs}",
-    "arn:aws:s3:::${var.s3_bucket_ssm_logs}/*"]
+      "arn:aws:s3:::${var.s3_bucket_ssm_logs}/*"
+    ]
   }
 
 
   statement {
-    sid    = "AllowAccessToKmsKeyForSsmLogging"
-    effect = "Allow"
+    sid     = "AllowAccessToKmsKeyForSsmLogging"
+    effect  = "Allow"
     actions = [
       "kms:Decrypt"
     ]
