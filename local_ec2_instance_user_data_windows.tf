8db0b08d45c1cbc80164df1146cc99c80f8cc38f
Samuel Loh Yin Kang <samuel.loh.yk@gmail.com>
2022-10-23 20:17:39 +0800

    Fixing template file names
    Signed-off-by: Samuel Loh Yin Kang <samuel.loh.yk@gmail.com>

--- local_ec2_instance_user_data_windows.tf
+++ local_ec2_instance_user_data_windows.tf
@@ -1,10 +1,10 @@
 locals {
   user_data_windows = join("\n", [
     "<powershell>",
-    templatefile("${path.module}/install-ssm-agent.ps1", {
+    templatefile("${path.module}/install-ssm-agent.ps1.tftpl", {
       aws_region_current = data.aws_region.current.name
     }),
-    templatefile("${path.module}/install-cloudwatch-agent.ps1.tpl", {
+    templatefile("${path.module}/install-cloudwatch-agent.ps1.tftpl", {
       aws_region_current    = data.aws_region.current.name
       cloudwatch_agent_json = local.cloudwatch_agent_config_windows_json
     }),
