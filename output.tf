52b9a155596e149cb29560991184a5e8d9d461ed
Samuel Loh Yin Kang <samuel.loh.yk@gmail.com>
2022-10-24 09:15:14 +0800

    Adding EC2 outputs
    Signed-off-by: Samuel Loh Yin Kang <samuel.loh.yk@gmail.com>

--- output.tf
+++ output.tf
@@ -10,3 +10,15 @@ output "instance_profile_iam_role_arn" {
 output "instance_id" {
   value = aws_instance.main.id
 }
+
+output "private_ip" {
+  value = aws_instance.main.private_ip
+}
+
+output "public_ip" {
+  value = aws_instance.main.public_ip
+}
+
+output "password_data" {
+  value = aws_instance.main.password_data
+}
